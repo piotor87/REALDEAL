d83cf94f22ae3c8928029eaee956be0887fecaa3
piotor87 <pietro.dellabriottaparolo@gmail.com>
2022-04-14 16:03:24 +0300

    changes

--- REALDEAL.py
+++ REALDEAL.py
@@ -5,27 +5,28 @@ import io
 import requests
 
 
-starting_real_deal = {"player":"Carlos Alcaraz",'age':18.8774811773}
-
-
+starting_real_deal = {"player":"Carlos Alcaraz",'date':20220101 }
+url="https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_matches_2022.csv"
+columns=["tourney_date","tourney_name","winner_name","winner_age",'loser_name','loser_age','round','score']
 
 def return_data():
-    url="https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_matches_2022.csv"
+
     s=requests.get(url).content
-    c=pd.read_csv(io.StringIO(s.decode('utf-8')),index_col=None)
+    c=pd.read_csv(io.StringIO(s.decode('utf-8')),index_col=None,usecols=columns)
     return c
-
-
 
 def return_losses(df,current_real_deal=starting_real_deal):
 
-    rd = df[df["loser_name"] ==starting_real_deal['player']]
-    loss_age = rd.loser_age.max()
-    if loss_age < current_real_deal['age']:
-        print("REAL DEAL UNCHANGED")
-    return rd
-    
+    rd_df = filter_df(df,starting_real_deal["player"],starting_real_deal["date"])
+    real_deal =[]
+    while not rd_df.empty:
+        #get loss with lowest age (can only be one)
+        loss = rd_df.loc[rd_df['tourney_date'].idxmin()]
+        real_deal.append(loss.to_dict())
+        rd_df = filter_df(df,loss.winner_name,loss.tourney_date)
+        
+    return real_deal
 
-
-
-    
+def filter_df(df,name,date):
+    print(f"filtering earliest loss of {name} after date {date}")
+    return df[(df.loser_name == name) & (df.tourney_date > date)]
